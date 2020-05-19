c417fe282ac94b1fa8817fc9bd347d21fa7ee300
hammingchok <isxmyoo@163.com>
2020-05-19 15:38:49 +0800

    update block queue

--- WebServer/block_queue.h
+++ WebServer/block_queue.h
@@ -1,7 +1,7 @@
-/*************************************************************
-*循环数组实现的阻塞队列，m_back = (m_back + 1) % m_max_size;  
-*线程安全，每个操作前都要先加互斥锁，操作完后，再解锁
-**************************************************************/
+/*
+ * 循环数组实现的阻塞队列，m_back = (m_back + 1) % m_max_size;  
+ * 使用互斥锁实现的线程安全，每个操作前都要先加互斥锁，操作完后，再解锁
+*/
 
 #ifndef BLOCK_QUEUE_H
 #define BLOCK_QUEUE_H
@@ -25,7 +25,7 @@ public:
         }
 
         m_max_size = max_size;
-        m_array = new T[max_size];
+        m_array = new T[max_size]; //数组保存
         m_size = 0;
         m_front = -1;
         m_back = -1;
@@ -48,6 +48,7 @@ public:
 
         m_mutex.unlock();
     }
+    
     //判断队列是否满了
     bool full() 
     {
@@ -61,6 +62,7 @@ public:
         m_mutex.unlock();
         return false;
     }
+    
     //判断队列是否为空
     bool empty() 
     {
@@ -73,6 +75,7 @@ public:
         m_mutex.unlock();
         return false;
     }
+    
     //返回队首元素
     bool front(T &value) 
     {
@@ -86,6 +89,7 @@ public:
         m_mutex.unlock();
         return true;
     }
+    
     //返回队尾元素
     bool back(T &value) 
     {
@@ -121,6 +125,7 @@ public:
         m_mutex.unlock();
         return tmp;
     }
+    
     //往队列添加元素，需要将所有使用队列的线程先唤醒
     //当有元素push进队列,相当于生产者生产了一个元素
     //若当前没有线程等待条件变量,则唤醒无意义
@@ -143,14 +148,17 @@ public:
 
         m_cond.broadcast();
         m_mutex.unlock();
-        return true;
+        
+	return true;
     }
+    
     //pop时,如果当前队列没有元素,将会等待条件变量
     bool pop(T &item)
     {
 
         m_mutex.lock();
-        while (m_size <= 0)
+        
+	while (m_size <= 0)
         {
             
             if (!m_cond.wait(m_mutex.get()))
@@ -163,8 +171,10 @@ public:
         m_front = (m_front + 1) % m_max_size;
         item = m_array[m_front];
         m_size--;
-        m_mutex.unlock();
-        return true;
+        
+	m_mutex.unlock();
+        
+	return true;
     }
 
     //增加了超时处理
@@ -173,8 +183,10 @@ public:
         struct timespec t = {0, 0};
         struct timeval now = {0, 0};
         gettimeofday(&now, NULL);
-        m_mutex.lock();
-        if (m_size <= 0)
+        
+	m_mutex.lock();
+        
+	if (m_size <= 0)
         {
             t.tv_sec = now.tv_sec + ms_timeout / 1000;
             t.tv_nsec = (ms_timeout % 1000) * 1000;
@@ -194,8 +206,10 @@ public:
         m_front = (m_front + 1) % m_max_size;
         item = m_array[m_front];
         m_size--;
-        m_mutex.unlock();
-        return true;
+        
+	m_mutex.unlock();
+        
+	return true;
     }
 
 private:

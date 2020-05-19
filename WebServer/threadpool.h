75925d00fb02457d82acdc98e015e2b10000c873
hammingchok <isxmyoo@163.com>
2020-05-19 16:48:22 +0800

    modify threadpool

--- WebServer/threadpool.h
+++ WebServer/threadpool.h
@@ -12,35 +12,44 @@ template <typename T>
 class threadpool
 {
 public:
-    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
+    //thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量
     threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
     ~threadpool();
     bool append(T *request, int state);
     bool append_p(T *request);
 
 private:
-    /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
+    //工作线程运行的函数，它不断从工作队列中取出任务并执行之
     static void *worker(void *arg);
     void run();
 
 private:
     int m_thread_number;        //线程池中的线程数
     int m_max_requests;         //请求队列中允许的最大请求数
+    
     pthread_t *m_threads;       //描述线程池的数组，其大小为m_thread_number
-    std::list<T *> m_workqueue; //请求队列
+    
+    std::list<T *> m_workqueue; //请求队列（由双向链表实现）
     locker m_queuelocker;       //保护请求队列的互斥锁
+    
     sem m_queuestat;            //是否有任务需要处理
     connection_pool *m_connPool;  //数据库
     int m_actor_model;          //模型切换
 };
+
 template <typename T>
 threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int thread_number, int max_requests) : m_actor_model(actor_model),m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL),m_connPool(connPool)
 {
     if (thread_number <= 0 || max_requests <= 0)
         throw std::exception();
+    
+    //初始化线程池，保存线程id
     m_threads = new pthread_t[m_thread_number];
+    
     if (!m_threads)
         throw std::exception();
+
+    //
     for (int i = 0; i < thread_number; ++i)
     {
         if (pthread_create(m_threads + i, NULL, worker, this) != 0)
@@ -55,40 +64,54 @@ threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int threa
         }
     }
 }
+
 template <typename T>
 threadpool<T>::~threadpool()
 {
     delete[] m_threads;
 }
+
+//线程同步
+//将事件放入队列中
 template <typename T>
 bool threadpool<T>::append(T *request, int state)
 {
+    //互斥锁：保护临界区
     m_queuelocker.lock();
-    if (m_workqueue.size() >= m_max_requests)
+    
+    if (m_workqueue.size() >= m_max_requests) //
     {
         m_queuelocker.unlock();
         return false;
     }
     request->m_state = state;
     m_workqueue.push_back(request);
+    
     m_queuelocker.unlock();
-    m_queuestat.post();
+   
+    m_queuestat.post(); //信号量唤醒
     return true;
 }
+
+//线程同步
 template <typename T>
 bool threadpool<T>::append_p(T *request)
 {
     m_queuelocker.lock();
+    
     if (m_workqueue.size() >= m_max_requests)
     {
         m_queuelocker.unlock();
         return false;
     }
     m_workqueue.push_back(request);
+    
     m_queuelocker.unlock();
     m_queuestat.post();
+    
     return true;
 }
+
 template <typename T>
 void *threadpool<T>::worker(void *arg)
 {
@@ -96,24 +119,29 @@ void *threadpool<T>::worker(void *arg)
     pool->run();
     return pool;
 }
+
 template <typename T>
 void threadpool<T>::run()
 {
     while (true)
     {
-        m_queuestat.wait();
-        m_queuelocker.lock();
-        if (m_workqueue.empty())
+        m_queuestat.wait(); //信号量wait
+        m_queuelocker.lock(); //互斥锁
+        
+	if (m_workqueue.empty())
         {
             m_queuelocker.unlock();
             continue;
         }
-        T *request = m_workqueue.front();
+        
+	T *request = m_workqueue.front(); //操作双向链表
         m_workqueue.pop_front();
-        m_queuelocker.unlock();
-        if (!request)
+        
+	m_queuelocker.unlock();
+        
+	if (!request)
             continue;
-        if (1 == m_actor_model)
+        if (1 == m_actor_model) //reactor
         {
             if (0 == request->m_state)
             {
